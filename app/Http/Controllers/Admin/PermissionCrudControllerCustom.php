38729a5673a81e48f5b19a4b0ac84703c41190e2
amadio5 <33965068+amadio5@users.noreply.github.com>
2018-01-29 18:40:36 +0100

    m

--- app/Http/Controllers/Admin/PermissionCrudControllerCustom.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\Admin;
-
-use Backpack\PermissionManager\app\Http\Controllers\PermissionCrudController;
-
-class PermissionCrudControllerCustom extends PermissionCrudController
-{
-}
--- app/Http/Controllers/Admin/RoleCrudControllerCustom.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\Admin;
-
-use Backpack\PermissionManager\app\Http\Controllers\RoleCrudController;
-
-class RoleCrudControllerCustom extends RoleCrudController
-{
-}
